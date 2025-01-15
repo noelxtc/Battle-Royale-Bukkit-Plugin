393d1d6bf3d71d5da50f6b148ce6674d67457f8b
NoeliSnOtCool <noeltonychangankeril@gmail.com>
2025-01-15 09:12:12 +0100

    Update CatastrophyService.java
    Updated author

--- src/main/java/io/github/noelxtc/battleroyale/catastrophy/CatastrophyService.java
+++ src/main/java/io/github/noelxtc/battleroyale/catastrophy/CatastrophyService.java
@@ -1,4 +1,10 @@
+package io.github.noelxtc.battleroyale.catastrophy;
 
+import io.github.noelxtc.battleroyale.catastrophy.impl.BossSummon;
+import io.github.noelxtc.battleroyale.catastrophy.impl.Flood;
+import io.github.noelxtc.battleroyale.catastrophy.impl.LandDestruction;
+import io.github.noelxtc.battleroyale.catastrophy.impl.LightningStorm;
+import io.github.noelxtc.battleroyale.catastrophy.impl.NetherInvasion;
 
 import java.util.ArrayList;
 import java.util.List;
